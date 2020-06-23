0968787fcd9c2144894ad4dd4787f7597a606f17
shawavisek35 <shawavisek35@gmail.com>
2020-06-23 11:39:10 +0530

    done

--- src/components/dishDetailComponent/dishDetailComponent.js
+++ src/components/dishDetailComponent/dishDetailComponent.js
@@ -1,58 +1,63 @@
 import React , {Component} from "react";
 
-class DishDetail extends Component {
+function RenderComments(comments){
+    return (
+        <div className="mb-5">
+            <li>
+                {comments.comment}
+            </li>
+            <p className="h6">--{comments.author} , {comments.date}</p>
+        </div>
+    )
+}
 
-    constructor(props){
-        super(props);
-        this.state = {
-        
-        }
-    }
-    render() {
-        
-            
-            if(this.props.selectedDish!=null){
-                const comments = this.props.selectedDish.comments.map((comment,index) => {
-                    return (
-                        <div key={index} className="mb-5">
-                            <li key={index}>
-                                {comment.comment}
-                            </li>
-                            <p>--{comment.author} , {comment.date}</p>
-                        </div>
+function RenderDish(dish){
+    return (
+        <div className="col-md-6 col-sm-12">
+            <div class="card mb-5" style={{width: "25rem"}}>
+                <img src={dish.image} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h4 class="card-title">{dish.name}</h4>
+                    <h5>Price : ${dish.price}</h5>
+                    <p class="card-text">{dish.description}</p>
+                            
+                </div>
+            </div>
+        </div>        
+    );
+}
+
+const  DishDetail = (props) => {
+
+    if(props.selectedDish!=null){
+        const comments = props.selectedDish.comments.map((comment,index) => {
+                return (
+                    <div key={index} className="mb-5">
+                        {RenderComments(comment)}
+                    </div>
                         
-                    )
-                });
-                return (
-                    <div className="row">
-                    <div className="col-md-6 col-sm-12">
-                        <div class="card mb-5" style={{width: "25rem"}}>
-                            <img src={this.props.selectedDish.image} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h4 class="card-title">{this.props.selectedDish.name}</h4>
-                                <h5>Price : ${this.props.selectedDish.price}</h5>
-                                <p class="card-text">{this.props.selectedDish.description}</p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-sm-12">
-                        <h4 className="mb-4 text-center">Comments</h4>
-                        <ul>
-                            {comments}
-                        </ul>
-                    </div>
-                    </div>
-                )
-            }
-            else{
-                return (
-                    <div></div>
-                )
-            }
+                );
+        });
+        return (
+            <div className="row">
+                {RenderDish(props.selectedDish)}
+                <div className="col-md-6 col-sm-12">
+                    <h4 className="mb-4 text-center">Comments</h4>
+                    <ul>
+                        {comments}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div></div>
+        )
+    }
             
         
-    }
+    
 }
 
 
